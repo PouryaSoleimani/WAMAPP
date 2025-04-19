ade0a61501f472c1b1c66430eaf8acac8e3b1354
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-20 02:36:44 +0330

    GRID BOXES

--- src/components/modules/CategoryBoxComponent.tsx
+++ src/components/modules/CategoryBoxComponent.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactLogo from "../../public/image2.png"
 
+interface CategoryBoxComponentProps { title: string }
 
-
-const CategoryBoxComponent = () => {
+const CategoryBoxComponent = ({ id, title }) => {
     return (
         <div className='border-2 border-green-800 rounded-xl bg-white text-black h-44 flex flex-col justify-between hover:scale-110 duration-300'>
             <img src={ReactLogo} alt='Category' width={90} className='rounded-t-lg' />
-            <h2 className='py-5 font-bold tracking-tighter'>لوازم فروشی</h2>
+            <h2 className='mb-4 font-bold tracking-tighter'>{title}</h2>
         </div>
     )
 }
--- src/components/templates/CategoryGridSection.tsx
+++ src/components/templates/CategoryGridSection.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import CategoryBoxComponent from '../modules/CategoryBoxComponent'
 
+const boxDatas = [
+    { id: 1, title: "لوازم فروشی" },
+    { id: 2, title: "معلم" },
+    { id: 3, title: "داروخانه" },
+    { id: 4, title: "فریلنسری" },
+    { id: 5, title: "سرگرمی و مدیا" },
+    { id: 6, title: "شبکه اجتماعی" },
+    { id: 7, title: "بازار مالی" },
+    { id: 8, title: "ورزشی" },
+    { id: 9, title: "تحصیل" },
+]
 const CategoryGridSection = () => {
     return (
         <div className='border-4 border-red-800 GRID__PARENT'>
-           <CategoryBoxComponent/>
-           <CategoryBoxComponent/>
-           <CategoryBoxComponent/>
-           <CategoryBoxComponent/>
-           <CategoryBoxComponent/>
-           <CategoryBoxComponent/>
-           <CategoryBoxComponent/>
-           <CategoryBoxComponent/>
-           <CategoryBoxComponent/>
+            {boxDatas.map((item) => (
+                <CategoryBoxComponent {...item} />
+            ))}
+
         </div>
     )
 }
