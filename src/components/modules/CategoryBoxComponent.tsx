d80e321e129dca1d1d7c173df407902174d20fcc
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-20 02:30:33 +0330

    GRID BOXES

--- src/components/modules/CategoryBoxComponent.tsx
+++ src/components/modules/CategoryBoxComponent.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import ReactLogo from "../../public/image2.png"
+
+
 
 const CategoryBoxComponent = () => {
     return (
-        <div className='border-2 border-green-800 rounded-xl bg-white text-black h-44'>
-            <img src='../../images/image-1.jpg' alt="box-image" width={40} height={40} loading="lazy" />
+        <div className='border-2 border-green-800 rounded-xl bg-white text-black h-44 flex flex-col justify-between hover:scale-110 duration-300'>
+            <img src={ReactLogo} alt='Category' width={90} className='rounded-t-lg' />
+            <h2 className='py-5 font-bold tracking-tighter'>لوازم فروشی</h2>
         </div>
     )
 }
