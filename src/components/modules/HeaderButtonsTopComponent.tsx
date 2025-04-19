b13694f3b96e7dc764c8a0b9ea3a67058688fd9f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-20 01:11:28 +0330

    STYLE

--- src/components/modules/HeaderButtonsTopComponent.tsx
+++ src/components/modules/HeaderButtonsTopComponent.tsx
@@ -3,9 +3,9 @@ import { Button } from '../ui/button'
 
 const HeaderButtonsTopComponent = () => {
     return (
-        <div className='border border-green-600 w-full'>
-            <Button>HELLO2</Button>
-            <Button className='bg-white text-black text-2xl font-bold px-6 py-9 tracking-tighter rounded-xl w-1/3 hover:bg-zinc-300 duration-500 cursor-pointer'>لیست فروشنده ها</Button>
+        <div className='w-[80%] mx-auto my-6 flex items-center justify-center gap-5'>
+            <Button className='bg-white text-[var(--pink)] text-2xl font-bold px-6 py-9 tracking-tighter rounded-xl w-1/2 hover:bg-[var(--pink)] hover:text-white duration-500 cursor-pointer'>همین حالا شروع کن</Button>
+            <Button className='bg-white text-black text-2xl font-bold px-6 py-9 tracking-tighter rounded-xl w-1/2 hover:bg-zinc-500 hover:text-white duration-500 cursor-pointer'>لیست فروشنده ها</Button>
         </div>
     )
 }
