acd619dca3406bb6a18363d7f25389ed44dbc60e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-20 01:30:41 +0330

    STYLE

--- src/components/modules/HeaderTitleComponent.tsx
+++ src/components/modules/HeaderTitleComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeaderTitleComponent = () => {
     return (
         <div >
-            <h1 className='text-white font-black text-4xl bg-[#F92684] px-2 py-6 rounded-lg tracking-tighter'>دریافت اعتبار خرید کالا</h1>
+            <h1 className='text-white font-black text-4xl bg-[#F92684] w-fit px-6 mx-auto py-6 rounded-lg tracking-tighter'>دریافت اعتبار خرید کالا</h1>
         </div>
     )
 }
