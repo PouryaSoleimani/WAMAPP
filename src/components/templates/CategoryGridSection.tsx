d0fa9181923a7919f011608a5f0a8be74acedaec
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-20 01:34:57 +0330

    STYLE

---
+++ src/components/templates/CategoryGridSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryGridSection = () => {
+    return (
+        <div className='border border-red-800'>
+            CategoryGridSection
+        </div>
+    )
+}
+
+export default CategoryGridSection
